2fd37d56776d63babfb22ed17327935d677d0158
Tobi.King <tobiaskoenig1@googlemail.com>
2012-03-27 21:19:54 +0200

    fixed double sending of Blogentries

--- js/entries.js
+++ js/entries.js
@@ -18,32 +18,39 @@ $.get('PHP/entries.php?goid='+cur_oldestID+'&'+cur_searchString, function(data) 
 function submitentrie(){
 	$.get('PHP/getsession.php', function(data) {
 		if(data!="NULL" && data!=""){
-			var params = "guid="+data+"&gtxt="+convertToHtml($('#textwrite').val())+"&gtitle="+$('#titlewrite').val();
-
-			var url = "PHP/submitentrie.php";
-			var http = new XMLHttpRequest();
-			http.open("POST", url, true);
-			
-			//Send the proper header information along with the request
-			http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-			http.setRequestHeader("Content-length", params.length);
-			http.setRequestHeader("Connection", "close");
-			
-			http.onreadystatechange = function() {//Call a function when the state changes.
-				if(http.readyState == 4 && http.status == 200) {
-					$('#textwrite').val("");
-					$('#titlewrite').val("");
-					loadnewentries();
-					//alert(http.responseText);
+			if($('#textwrite').val() != "" && $('#titlewrite').val()!=""){
+				if(sending_entry==false){
+					sending_entry=true;
+					var params = "guid="+data+"&gtxt="+convertToHtml($('#textwrite').val())+"&gtitle="+$('#titlewrite').val();
+		
+					var url = "PHP/submitentrie.php";
+					var http = new XMLHttpRequest();
+					http.open("POST", url, true);
+					
+					//Send the proper header information along with the request
+					http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+					http.setRequestHeader("Content-length", params.length);
+					http.setRequestHeader("Connection", "close");
+					
+					http.onreadystatechange = function() {//Call a function when the state changes.
+						if(http.readyState == 4 && http.status == 200) {
+							$('#textwrite').val("");
+							$('#titlewrite').val("");
+							sending_entry=false;
+							loadnewentries();
+							//alert(http.responseText);
+						}
+					};
+					
+					http.send(params);
 				}
-			};
-			
-			http.send(params);
+			}
+			else{
+				alert("You cannot submit an empty text!");
+			}
 		}
 		else{
 			alert("You are not logged in");
 		}
 	});
 }
-
-	
